3b05cda9e4f36e2f73b2843a254ac72564ec3a2a
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-07-30 16:39:49 -0300

    feat: add dependency injection concepts

---
+++ src/main/java/com/algaworks/erp/id/Pedidos.java
@@ -0,0 +1,10 @@
+package com.algaworks.erp.id;
+
+import java.math.BigDecimal;
+
+public class Pedidos {
+
+	public BigDecimal totalPedidosMesAtual() {
+		return new BigDecimal("100");
+	}
+}
---
+++ src/main/java/com/algaworks/erp/id/RelatorioService.java
@@ -0,0 +1,26 @@
+package com.algaworks.erp.id;
+
+import java.math.BigDecimal;
+
+import javax.inject.Inject;
+
+public class RelatorioService {
+
+	@Inject
+	private Pedidos pedidos;
+
+	public RelatorioService(Pedidos pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	public RelatorioService() {
+	}
+	
+	public BigDecimal totalPedidosMesAtual() {
+		return pedidos.totalPedidosMesAtual();
+	}
+	
+	public void setPedidos(Pedidos pedidos) {
+		this.pedidos = pedidos;
+	}
+}
