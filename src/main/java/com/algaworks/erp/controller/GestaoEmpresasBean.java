db5b236a02cf68fd0ef81483be9a4804131bb206
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-08-01 15:41:33 -0300

    refactor: remove unnecessary file and organize project

--- src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -6,7 +6,6 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import com.algaworks.erp.model.Empresa;
-import com.algaworks.erp.model.TipoEmpresa;
 
 @Named
 @ViewScoped
@@ -15,23 +14,6 @@ public class GestaoEmpresasBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Empresa empresa = new Empresa();
-
-	public void salvar() {
-		System.out.println("Razão social: " + empresa.getRazaoSocial() + " - Nome fantasia: "
-				+ empresa.getNomeFantasia() + " - Tipo: " + empresa.getTipo() + " - Data Fundação: "
-				+ empresa.getDataFundacao() + " - Faturamento: " + empresa.getFaturamento());
-	}
-
-	public Empresa getEmpresa() {
-		return empresa;
-	}
-
-	public TipoEmpresa[] getTiposEmpresa() {
-		return TipoEmpresa.values();
-	}
-
-	public String ajuda() {
-		return "AjudaGestaoEmpresas?faces-redirect=true";
-	}
-
+	
+	
 }
