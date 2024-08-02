7bdf9287129ce46ebc2a93aaaa81062c5a3621b2
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-08-01 21:50:46 -0300

    feat: add companies list with data table

--- src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -1,11 +1,14 @@
 package com.algaworks.erp.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.algaworks.erp.model.Empresa;
+import com.algaworks.erp.repository.Empresas;
 
 @Named
 @ViewScoped
@@ -13,7 +16,16 @@ public class GestaoEmpresasBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private Empresa empresa = new Empresa();
-	
-	
+	@Inject
+	private Empresas empresas;
+
+	private List<Empresa> listaEmpresas;
+
+	public void todasEmpresas() {
+		listaEmpresas = empresas.todas();
+	}
+
+	public List<Empresa> getListaEmpresas() {
+		return listaEmpresas;
+	}
 }
--- src/main/java/com/algaworks/erp/util/EntityManagerProducer.java
+++ src/main/java/com/algaworks/erp/util/EntityManagerProducer.java
@@ -24,7 +24,7 @@ public class EntityManagerProducer implements Serializable {
 	@Produces
 	@RequestScoped
 	public EntityManager createEntityManager() {
-		return this.createEntityManager();
+		return this.factory.createEntityManager();
 	}
 	
 	public void closeEntityManager(@Disposes EntityManager manager) {
